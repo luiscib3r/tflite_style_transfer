d0b8b1c1e6455f389081a01ddc2e69cc67697a2e
Luis Ciber <luisciber640@gmail.com>
2022-09-01 16:08:40 -0400

    feat(tflite_style_transfer_android): implement runStyleTransfer method for Android

--- packages/tflite_style_transfer/example/android/app/src/main/kotlin/dev/luisciber/tflite_style_transfer_example/MainActivity.kt
+++ packages/tflite_style_transfer/example/android/app/src/main/kotlin/dev/luisciber/tflite_style_transfer_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.luisciber.example
+package dev.luisciber.tflite_style_transfer_example
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ packages/tflite_style_transfer_android/android/src/main/kotlin/dev/luisciber/ModelResult.kt
@@ -0,0 +1,14 @@
+package dev.luisciber
+
+import android.graphics.Bitmap
+
+data class ModelResult(
+    val styledImage: Bitmap,
+    val preProcessTime: Long = 0L,
+    val stylePredictTime: Long = 0L,
+    val styleTransferTime: Long = 0L,
+    val postProcessTime: Long = 0L,
+    val totalExecutionTime: Long = 0L,
+    val executionLog: String = "",
+    val errorMessage: String = ""
+)
